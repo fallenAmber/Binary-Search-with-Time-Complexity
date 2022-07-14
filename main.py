732b8e59a425835d46204b31122f53b2199f1dba
Md Tohidul Islam <mdtohidulislam@Mds-MacBook-Air.local>
2022-07-14 12:05:53 +0600

    First Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+l = [1,2,3,4,5,6,7,8]
+
+print(len(l)//2)
