ff14d2d0130f3d3f7355bb92d09ca1e62461ca69
Md Tohidul Islam <mdtohidulislam@Mds-MacBook-Air.local>
2022-07-14 12:18:12 +0600

    Second Commit

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+import math
+import time
+
+def naive_search(l, target):
+
+    for i in range(len(l)):
+
+        if l[i]==target:
+            return f"{target} is in the list"
+
+    return -1
+
+def BinarySearch(l, target, low=None, high=None):
+
+    if low is None:
+        low =0
+    if high is None:
+        high = len(l)-1
+
+    midpoint = (high+low)//2
+
+    if target==l[midpoint]:
+        return midpoint
+
+    elif  target < l[midpoint]:
+        return BinarySearch(l, target, low, midpoint-1)
+    elif target > l[midpoint]:
+        return BinarySearch(l, target, midpoint+1, high)
 
 
-l = [1,2,3,4,5,6,7,8]
+if __name__ == '__main__':
+    length = 10000
+    sorted_list = set()
 
-print(len(l)//2)
+    while len(sorted_list) < length:
+        sorted_list.add(random.randint(-3 * length, 3 * length))
+    sorted_list = sorted(list(sorted_list))
+
+    start = time.time()
+    for target in sorted_list:
+        naive_search(sorted_list, target)
+    end = time.time()
+    print("Naive search time: ", (end - start), "seconds")
+
+    start = time.time()
+    for target in sorted_list:
+        BinarySearch(sorted_list, target)
+    end = time.time()
+    print("Binary search time: ", (end - start), "seconds")
+
